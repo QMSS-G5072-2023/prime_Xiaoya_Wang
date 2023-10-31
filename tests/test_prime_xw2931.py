ea9f11c1aa545b8d451fc48261557b9861802c66
RRWXY <144049508+RRWXY@users.noreply.github.com>
2023-11-01 03:48:37 +0800

    1

--- tests/test_prime_xw2931.py
+++ tests/test_prime_xw2931.py
@@ -1,1 +1,11 @@
 from prime_xw2931 import prime_xw2931
+import sys
+import os
+
+
+def test_is_prime():
+    assert is_prime(2) is True
+    assert is_prime(11) is True
+    assert is_prime(4) is False
+    assert is_prime(1) is False
+    assert is_prime(0) is False
